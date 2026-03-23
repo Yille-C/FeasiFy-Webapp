b75e519def0942cfe80e6a3095610678e0a50850
Yille-C <kehlkairuuu@gmail.com>
2026-03-23 13:23:47 +0800

    Refactor Auth component for improved readability

--- src/Auth.tsx
+++ src/Auth.tsx
@@ -20,13 +20,7 @@ const Auth: React.FC = () => {
 
   const navigate = useNavigate();
 
-  const activePassword = isLogin ? loginForm.password : registerForm.password;
-  const passwordChecks = {
-    length: activePassword.length >= 8 && activePassword.length <= 20,
-    upper: /[A-Z]/.test(activePassword),
-    lower: /[a-z]/.test(activePassword),
-    number: /\d/.test(activePassword),
-  };
+
 
   const strengthLabel = !isLogin
     ? activePassword.length >= 21
